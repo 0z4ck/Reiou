1d28445108923d80891584d6a21732a6832b9cdc
0z4ck <z4ck@m.z4ck.tk>
2021-04-22 21:10:41 +0200

    silver drop implemented

--- Assets/gin.cs
+++ Assets/gin.cs
@@ -10,262 +10,284 @@ public class gin : Chessman
 
         if (isWhite)
         {
-
-            Chessman c;
-
-
-
-            /*
-            // Top Side
-            i = CurrentX - 1;
-            j = CurrentY + 1;
-            if (CurrentY != 8)
+            if (CurrentY == -1)
             {
-
-                Debug.Log(CurrentX);
-                for (int k = 0; k < 3; k++)
-                {
-                    
-                    if (i >= 0 || i < 9)
-                    {
-                        c = BoradManager.Instance.Chessmans[i, j];
-                        if (c == null)
+                for (int i = 0; i < 9; i++)
+                    for (int j = 0; j < 9; j++)
+                        if (BoradManager.Instance.Chessmans[i, j] == null)
                             r[i, j] = true;
-                        else if (isWhite != c.isWhite)
-                            r[i, j] = true;
-                    }
 
-                    i++;
-                }
             }
-            */
-
-            // Up Left
-            if (CurrentY != 8)
+            else
             {
+
+                Chessman c;
+
+
+
+                /*
+                // Top Side
+                i = CurrentX - 1;
+                j = CurrentY + 1;
+                if (CurrentY != 8)
+                {
+
+                    Debug.Log(CurrentX);
+                    for (int k = 0; k < 3; k++)
+                    {
+
+                        if (i >= 0 || i < 9)
+                        {
+                            c = BoradManager.Instance.Chessmans[i, j];
+                            if (c == null)
+                                r[i, j] = true;
+                            else if (isWhite != c.isWhite)
+                                r[i, j] = true;
+                        }
+
+                        i++;
+                    }
+                }
+                */
+
+                // Up Left
+                if (CurrentY != 8)
+                {
+                    if (CurrentX != 0)
+                    {
+                        //
+                        c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY + 1];
+                        if (c == null)
+                        {
+                            r[CurrentX - 1, CurrentY + 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX - 1, CurrentY + 1] = true;
+                    }
+                }
+
+                // Up Right
+                if (CurrentY != 8)
+                {
+                    if (CurrentX != 8)
+
+                    {
+                        c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY + 1];
+                        if (c == null)
+                        {
+                            r[CurrentX + 1, CurrentY + 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX + 1, CurrentY + 1] = true;
+                    }
+                }
+
+
+
+                //Up
+                if (CurrentY != 8)
+                {
+                    c = BoradManager.Instance.Chessmans[CurrentX, CurrentY + 1];
+                    if (c == null)
+                        r[CurrentX, CurrentY + 1] = true;
+                    else if (isWhite != c.isWhite)
+                        r[CurrentX, CurrentY + 1] = true;
+                }
+
+
+                // Down Left
+                if (CurrentY != 0)
+                {
+                    if (CurrentX != 0)
+                    {
+                        //
+                        c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY - 1];
+                        if (c == null)
+                        {
+                            r[CurrentX - 1, CurrentY - 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX - 1, CurrentY - 1] = true;
+                    }
+                }
+
+                // Down Right
+                if (CurrentY != 0)
+                {
+                    if (CurrentX != 8)
+                    {
+                        c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY - 1];
+                        if (c == null)
+                        {
+                            r[CurrentX + 1, CurrentY - 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX + 1, CurrentY - 1] = true;
+                    }
+                }
+
+                /*
+                // Down Side
+                i = CurrentX - 1;
+                j = CurrentY - 1;
+                if (CurrentY != 0)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (i >= 0 || i < 9)
+                        {
+                            c = BoradManager.Instance.Chessmans[i, j];
+                            if (c == null)
+                                r[i, j] = true;
+                            else if (isWhite != c.isWhite)
+                                r[i, j] = true;
+                        }
+
+                        i++;
+                    }
+                }
+                */
+
+                /*
+                // Middle Left
                 if (CurrentX != 0)
                 {
-                    //
-                    c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY + 1];
+                    c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY];
                     if (c == null)
-                    {
-                        r[CurrentX - 1, CurrentY + 1] = true;
-                    }
+                        r[CurrentX - 1, CurrentY] = true;
                     else if (isWhite != c.isWhite)
-                        r[CurrentX - 1, CurrentY + 1] = true;
+                        r[CurrentX - 1, CurrentY] = true;
                 }
-            }
 
-            // Up Right
-            if (CurrentY != 8)
-            {
-                if (CurrentX != 8)
-
-                {
-                    c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY + 1];
-                    if (c == null)
-                    {
-                        r[CurrentX + 1, CurrentY + 1] = true;
-                    }
-                    else if (isWhite != c.isWhite)
-                        r[CurrentX + 1, CurrentY + 1] = true;
-                }
-            }
-
-
-
-            //Up
-            if (CurrentY != 8)
-            {
-                c = BoradManager.Instance.Chessmans[CurrentX, CurrentY + 1];
-                if (c == null)
-                    r[CurrentX, CurrentY + 1] = true;
-                else if (isWhite != c.isWhite)
-                    r[CurrentX, CurrentY + 1] = true;
-            }
-
-
-            // Down Left
-            if (CurrentY != 0)
-            {
-                if (CurrentX != 0)
-                {
-                    //
-                    c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY - 1];
-                    if (c == null)
-                    {
-                        r[CurrentX - 1, CurrentY - 1] = true;
-                    }
-                    else if (isWhite != c.isWhite)
-                        r[CurrentX - 1, CurrentY - 1] = true;
-                }
-            }
-
-            // Down Right
-            if (CurrentY != 0)
-            {
+                // Middle Right
                 if (CurrentX != 8)
                 {
-                    c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY - 1];
+                    c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY];
                     if (c == null)
-                    {
-                        r[CurrentX + 1, CurrentY - 1] = true;
-                    }
+                        r[CurrentX + 1, CurrentY] = true;
                     else if (isWhite != c.isWhite)
-                        r[CurrentX + 1, CurrentY - 1] = true;
+                        r[CurrentX + 1, CurrentY] = true;
                 }
+                */
             }
-
-            /*
-            // Down Side
-            i = CurrentX - 1;
-            j = CurrentY - 1;
-            if (CurrentY != 0)
-            {
-                for (int k = 0; k < 3; k++)
-                {
-                    if (i >= 0 || i < 9)
-                    {
-                        c = BoradManager.Instance.Chessmans[i, j];
-                        if (c == null)
-                            r[i, j] = true;
-                        else if (isWhite != c.isWhite)
-                            r[i, j] = true;
-                    }
-
-                    i++;
-                }
-            }
-            */
-
-            /*
-            // Middle Left
-            if (CurrentX != 0)
-            {
-                c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY];
-                if (c == null)
-                    r[CurrentX - 1, CurrentY] = true;
-                else if (isWhite != c.isWhite)
-                    r[CurrentX - 1, CurrentY] = true;
-            }
-
-            // Middle Right
-            if (CurrentX != 8)
-            {
-                c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY];
-                if (c == null)
-                    r[CurrentX + 1, CurrentY] = true;
-                else if (isWhite != c.isWhite)
-                    r[CurrentX + 1, CurrentY] = true;
-            }
-            */
         }
 
         else
         {
+            if (CurrentY == -1)
+            {
+                for (int i = 0; i < 9; i++)
+                    for (int j = 0; j < 9; j++)
+                        if (BoradManager.Instance.Chessmans[i, j] == null)
+                            r[i, j] = true;
 
-            Chessman c;
-
-
-
-            /*
-            // Top Side
-            i = CurrentX - 1;
-            j = CurrentY + 1;
-            if (CurrentY != 8)
+            }
+            else
             {
 
-                Debug.Log(CurrentX);
-                for (int k = 0; k < 3; k++)
+                Chessman c;
+
+
+
+                /*
+                // Top Side
+                i = CurrentX - 1;
+                j = CurrentY + 1;
+                if (CurrentY != 8)
                 {
-                    
-                    if (i >= 0 || i < 9)
+
+                    Debug.Log(CurrentX);
+                    for (int k = 0; k < 3; k++)
                     {
-                        c = BoradManager.Instance.Chessmans[i, j];
+
+                        if (i >= 0 || i < 9)
+                        {
+                            c = BoradManager.Instance.Chessmans[i, j];
+                            if (c == null)
+                                r[i, j] = true;
+                            else if (isWhite != c.isWhite)
+                                r[i, j] = true;
+                        }
+
+                        i++;
+                    }
+                }
+                */
+
+                // Teki Up Left
+                if (CurrentY != 8)
+                {
+                    if (CurrentX != 0)
+                    {
+                        //
+                        c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY + 1];
                         if (c == null)
-                            r[i, j] = true;
+                        {
+                            r[CurrentX - 1, CurrentY + 1] = true;
+                        }
                         else if (isWhite != c.isWhite)
-                            r[i, j] = true;
+                            r[CurrentX - 1, CurrentY + 1] = true;
                     }
-
-                    i++;
                 }
-            }
-            */
 
-            // Teki Up Left
-            if (CurrentY != 8)
-            {
-                if (CurrentX != 0)
+                // Up Right
+                if (CurrentY != 8)
                 {
-                    //
-                    c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY + 1];
-                    if (c == null)
+                    if (CurrentX != 8)
+
                     {
-                        r[CurrentX - 1, CurrentY + 1] = true;
+                        c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY + 1];
+                        if (c == null)
+                        {
+                            r[CurrentX + 1, CurrentY + 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX + 1, CurrentY + 1] = true;
                     }
-                    else if (isWhite != c.isWhite)
-                        r[CurrentX - 1, CurrentY + 1] = true;
                 }
-            }
 
-            // Up Right
-            if (CurrentY != 8)
-            {
-                if (CurrentX != 8)
 
+
+                //Up
+                if (CurrentY != 0)
                 {
-                    c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY + 1];
+                    c = BoradManager.Instance.Chessmans[CurrentX, CurrentY - 1];
                     if (c == null)
-                    {
-                        r[CurrentX + 1, CurrentY + 1] = true;
-                    }
+                        r[CurrentX, CurrentY - 1] = true;
                     else if (isWhite != c.isWhite)
-                        r[CurrentX + 1, CurrentY + 1] = true;
+                        r[CurrentX, CurrentY - 1] = true;
                 }
-            }
 
 
-
-            //Up
-            if (CurrentY != 0)
-            {
-                c = BoradManager.Instance.Chessmans[CurrentX, CurrentY - 1];
-                if (c == null)
-                    r[CurrentX, CurrentY - 1] = true;
-                else if (isWhite != c.isWhite)
-                    r[CurrentX, CurrentY - 1] = true;
-            }
-
-
-            // Down Left
-            if (CurrentY != 0)
-            {
-                if (CurrentX != 0)
+                // Down Left
+                if (CurrentY != 0)
                 {
-                    //
-                    c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY - 1];
-                    if (c == null)
+                    if (CurrentX != 0)
                     {
-                        r[CurrentX - 1, CurrentY - 1] = true;
+                        //
+                        c = BoradManager.Instance.Chessmans[CurrentX - 1, CurrentY - 1];
+                        if (c == null)
+                        {
+                            r[CurrentX - 1, CurrentY - 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX - 1, CurrentY - 1] = true;
                     }
-                    else if (isWhite != c.isWhite)
-                        r[CurrentX - 1, CurrentY - 1] = true;
                 }
-            }
 
-            // Down Right
-            if (CurrentY != 0)
-            {
-                if (CurrentX != 8)
+                // Down Right
+                if (CurrentY != 0)
                 {
-                    c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY - 1];
-                    if (c == null)
+                    if (CurrentX != 8)
                     {
-                        r[CurrentX + 1, CurrentY - 1] = true;
+                        c = BoradManager.Instance.Chessmans[CurrentX + 1, CurrentY - 1];
+                        if (c == null)
+                        {
+                            r[CurrentX + 1, CurrentY - 1] = true;
+                        }
+                        else if (isWhite != c.isWhite)
+                            r[CurrentX + 1, CurrentY - 1] = true;
                     }
-                    else if (isWhite != c.isWhite)
-                        r[CurrentX + 1, CurrentY - 1] = true;
                 }
             }
         }
