e8e1d871c2195adec3fb715799c16ad437748348
0z4ck <z4ck@m.z4ck.tk>
2021-04-28 17:10:02 +0200

    PlayerManager implemented

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using System.IO;
+using UnityEngine.SceneManagement;
 
-public class RoomManager : MonoBehaviour
+public class RoomManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
-    void Start()
+    public static RoomManager Instance;
+    private void Awake()
     {
-        
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEnable()
     {
-        
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
     }
 }
